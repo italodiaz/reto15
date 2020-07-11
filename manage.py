29f50a69698ab44347aa263d941f70dcaf23babf
Italo <italo.dtrm@gmail.com>
2020-07-11 11:11:56 -0500

    manage.py with env

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
